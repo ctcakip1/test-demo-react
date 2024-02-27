fb4c7a08031328d164a01a5390403e5f0d4164b3
ctcakip1 <tinigaming058@gmail.com>
2024-02-27 17:12:05 +0700

    m

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,32 +1,39 @@
 // class component
 // function component
 import React from "react";
+import { applyMiddleware } from "redux";
 class MyComponent extends React.Component {
   state = {
     name: "tuan anh anh",
     address: "bac ninh",
     age: 20,
   };
-  handleClick = (e) => {
-    console.log(e.target);
-    console.log("random", Math.floor(Math.random() * 100 + 1));
+  handleClick(e) {
+    console.log("click me button");
     // merge State  -> react class
     this.setState({
       name: "tuan em",
-      age: Math.floor(Math.random() * 100 + 1)
+      age: Math.floor(Math.random() * 100 + 1),
     });
-    
-  };
+  }
 
   handleOnMouseOver(e) {
     // console.log(e.pageX);
   }
+  handleOnChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and im from {this.state.age}
-        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
         <button
           onClick={(e) => {
             this.handleClick(e);
@@ -34,6 +41,10 @@ class MyComponent extends React.Component {
         >
           Click me
         </button>
+        <form onSubmit={(e)=>this.handleOnSubmit(e)}>
+          <input type="text" onChange={(e) => this.handleOnChange(e)} />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
